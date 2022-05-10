7922db81913289862fb1fc88e294d2b1d05c0f55
Tamiris Rezende Garcia Costa <tamirisrgarcia@gmail.com>
2022-05-09 23:18:58 -0300

    ajuste responsividade tela grande

--- main.js
+++ main.js
@@ -4,6 +4,38 @@ onScroll()
 function onScroll() {
     showNavOnScroll();
     showBackToTopButtonOnScroll();
+    activateMenuAtCurrentSection(home);
+    activateMenuAtCurrentSection(services);
+    activateMenuAtCurrentSection(about);
+    activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section){
+    const targetLine = scrollY + innerHeight / 2;
+
+    //verificar se a seção passou da linha alvo:
+    //quais dados eu vou precisar?
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+    console.log('O topo da seção passou do topo ou da linha alvo?', sectionTopReachOrPassedTargetLine)
+
+    // verificar se a seção está abaixo da linha alvo:
+    //quais dados eu vou precisar?
+    const sectionEndsAt = sectionTop + sectionHeight;
+    const sectionEndAndTargetLine = sectionEndsAt <= targetLine;
+    console.log('O fundo da seção passou da linha alvo?', sectionEndAndTargetLine)
+
+    //limites da seção:
+    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndAndTargetLine;
+
+    const sectionId = section.getAttribute('id');
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+    
+    menuElement.classList.remove('active');
+    if(sectionBoundaries) {
+        menuElement.classList.add('active');
+    }
 }
 
 function showNavOnScroll() {
